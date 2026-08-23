a2e2e1282e2d4b3d101f2508aa6f9fcf0544d468
SaridakisStamatisChristos <34583142+SaridakisStamatisChristos@users.noreply.github.com>
2026-08-23 23:59:19 +0300

    test: import TaskState in integration semantics fixtures

--- tests/test_integration_semantics_v12.py
+++ tests/test_integration_semantics_v12.py
@@ -8,7 +8,7 @@ import pytest
 from omarchy_yolo.config import Config, EngineConfig, GateConfig, GitConfig
 from omarchy_yolo.db import Database
 from omarchy_yolo.git import GitRepo
-from omarchy_yolo.model import GateResult, PlannedTask, ReviewResult
+from omarchy_yolo.model import GateResult, PlannedTask, ReviewResult, TaskState
 from omarchy_yolo.orchestrator import Orchestrator
 from omarchy_yolo.review_source import ReviewChunk
 from omarchy_yolo.util import YoloError
